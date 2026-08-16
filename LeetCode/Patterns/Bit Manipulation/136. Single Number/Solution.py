bf4581225858ac333ceb375372edb1f77022ad11
Ansh <sachinbest1234@gmail.com>
2026-08-16 16:29:10 +0530

    Add Python Solution to Patterns/Bit Manipulation

--- LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.py
+++ LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.py
@@ -1,12 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dict1 = {}
-
+        ans = 0
         for num in nums:
-            dict1[num] = dict1.get(num,  0)+1
+            ans = ans^num
 
-        for num in dict1:
-            if dict1[num] == 1:
-                return num
+        return ans 
+
 
         
