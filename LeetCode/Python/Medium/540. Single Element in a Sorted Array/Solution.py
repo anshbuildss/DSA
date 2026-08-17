4abef429620965a4b4107b9f89c52e5b4cb41409
Ansh <sachinbest1234@gmail.com>
2026-08-17 13:32:24 +0530

    Add Medium Python Solution from LeetCode

---
+++ LeetCode/Python/Medium/540. Single Element in a Sorted Array/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+
+        ans = 0 
+
+        for i in range(len(nums)):
+            ans = ans ^ nums[i]
+
+        return ans
+
+        
+        
