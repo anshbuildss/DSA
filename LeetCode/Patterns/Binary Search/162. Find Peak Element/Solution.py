303cd0587d5439f94fe194027f67c638fe3e977f
Ansh <sachinbest1234@gmail.com>
2026-08-18 13:29:46 +0530

    Add Python Solution to Patterns/Binary Search

---
+++ LeetCode/Patterns/Binary Search/162. Find Peak Element/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        low = 0 
+        high = n - 1
+
+        while low <= high:
+            mid = (low + high)//2
+
+            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+                return mid 
+
+            if nums[mid] > nums[mid-1]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1 
+        
