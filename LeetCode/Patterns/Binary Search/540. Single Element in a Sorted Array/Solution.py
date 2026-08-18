667eabc140a0b720f9d6043fac6bd0c74e2ca9ff
Ansh <sachinbest1234@gmail.com>
2026-08-18 13:17:48 +0530

    Add Python Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/540. Single Element in a Sorted Array/Solution.py
+++ LeetCode/Patterns/Binary Search/540. Single Element in a Sorted Array/Solution.py
@@ -1,12 +1,30 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
 
-        ans = 0 
+        n = len(nums)
 
-        for i in range(len(nums)):
-            ans = ans ^ nums[i]
+        if n == 1:
+            return nums[0]
+        if nums[0] != nums[1]:
+            return nums[0]
+        if nums[n-1] != nums[n-2]:
+            return nums[n-1]
 
-        return ans
+        low = 1
+        high = n-2
 
-        
+        while low <= high:
+            mid = (low + high)//2
+
+            if nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]:
+                return nums[mid]
+
+            if (mid % 2==1 and nums[mid] == nums[mid-1]) or (mid % 2 == 0 and nums[mid] == nums[mid+1]):
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return -1
+              
         
